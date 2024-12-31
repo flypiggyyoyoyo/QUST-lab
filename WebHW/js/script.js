52c47a43389aa9fb779be274428140da883795e7
QUSTflypiggy <150204110+QUSTflypiggy@users.noreply.github.com>
2024-12-31 14:37:07 +0800

    drop name

--- WebHW/js/script.js
+++ WebHW/js/script.js
@@ -5,11 +5,11 @@ const templates = document.getElementById("templates");
 
 menuItems.forEach(item => {
     item.addEventListener("click", function(event) {
-        event.preventDefault(); // 阻止默认行为
-        const targetId = this.getAttribute("data-target"); // 获取目标内容的 ID
-        const targetContent = templates.querySelector(`#${targetId}`); // 获取模板中的内容
+        event.preventDefault(); 
+        const targetId = this.getAttribute("data-target"); 
+        const targetContent = templates.querySelector(`#${targetId}`); 
         if (targetContent) {
-            contentArea.innerHTML = targetContent.innerHTML; // 更新内容框架
+            contentArea.innerHTML = targetContent.innerHTML; 
         }
     });
 });
